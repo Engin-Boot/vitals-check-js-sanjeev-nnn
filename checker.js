777d45acf908305c2ac55bf5e27b71f2ebac7031
sanjeev-nnn <69623115+sanjeev-nnn@users.noreply.github.com>
2020-08-28 00:09:32 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -23,5 +23,5 @@ expect(vitalsAreOk(10, 10, 70)).to.be.false;//bpm and spo2 fail
 expect(vitalsAreOk(100, 10, 10)).to.be.false;//spo2 and resprate fail
 expect(vitalsAreOk(10, 95, 10)).to.be.false;//bpm and resprate fail
 
-console.log(abnormalVitalReport(90,90,90));
+console.log(abnormalVitalReport(95,90,90));
 console.log('checker is done');
